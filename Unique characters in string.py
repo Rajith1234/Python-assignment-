20d9e1f41130d9d66d2044b0013334ca961d5f96
Rajith1234 <rajithraghavan03@gmail.com>
2024-11-10 20:30:41 +0530

    Create Unique characters in string.py

---
+++ Unique characters in string.py
@@ -0,0 +1,7 @@
+def unique_characters(input_string):
+    return ''.join(set(input_string))
+
+# Example usage:
+input_string = "programming"
+result = unique_characters(input_string)
+print(result)
